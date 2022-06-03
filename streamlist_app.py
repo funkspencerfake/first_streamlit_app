220efc310c800876951076f6a115b4f7ad279834
funkspencerfake <106833326+funkspencerfake@users.noreply.github.com>
2022-06-03 14:43:02 -0400

    Update streamlist_app.py
    Added a data frame menu

--- streamlist_app.py
+++ streamlist_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas as pd
 
 streamlit.title('My parents New Healthy Diner')
 
@@ -6,3 +7,8 @@ streamlit.header('Breakfast Favorites')
 streamlit.text(':oatmeal: Omega 3 & Blueberry Oatmeal')
 streamlit.text(':salad: Kale, Spinach and Rocket SMoothie')
 streamlit.text(':chicken: Hard-Boiled Free-Range Egg')
+
+
+my_fruit_list = pd.read_csv(https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt)
+
+streamlit.dataframe(my_fruit_list)
