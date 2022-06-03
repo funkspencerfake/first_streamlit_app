d9464b91edba221ebeff3362bfe4d1d4f08cd8ae
funkspencerfake <106833326+funkspencerfake@users.noreply.github.com>
2022-06-03 14:50:51 -0400

    Update streamlist_app.py
    fix typos

--- streamlist_app.py
+++ streamlist_app.py
@@ -13,6 +13,6 @@ my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/da
 my_fruit_list = my_fruit_list.set_index('Fruit', inplace=True)
 streamlit.header('Build your own smoothie!')
 
-streamlit.multiselect("Pick some Fruits", list(my_fruit_list.index), ['Avocado','Strawberry'])
+streamlit.multiselect("Pick some Fruits", list(my_fruit_list.index), ['Avocado','Strawberries'])
 
 streamlit.dataframe(my_fruit_list)
