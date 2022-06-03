b270d1021df9af387a7b0cfaa0d23144c13b0ef1
funkspencerfake <106833326+funkspencerfake@users.noreply.github.com>
2022-06-03 14:32:03 -0400

    Update streamlist_app.py
    Added menu items

--- streamlist_app.py
+++ streamlist_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My parents New Healthy Diner')
+
+streamlist.header('Breakfast Menu')
+streamlist.text('Omega 3 & Blueberry Oatmeal')
+streamlist.text('Kale, Spinach and Rocket SMoothie')
+streamlist.text('Hard-Boiled Free-Range Egg')
