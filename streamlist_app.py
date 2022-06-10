55f2b5fb7930d327c274a53bdd1e4c3fc4a12ced
funkspencerfake <106833326+funkspencerfake@users.noreply.github.com>
2022-06-10 09:16:31 -0400

    Add new section to display fruity vice api response

--- streamlist_app.py
+++ streamlist_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas as pd
+import requests
 
 streamlit.title('My parents New Healthy Diner')
 
@@ -18,3 +19,7 @@ fruits_selected = streamlit.multiselect("Pick some Fruits", list(my_fruit_list.i
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
+
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
