6fb10bf08c791e63e3cf02b4cc3a8736dddf251a
funkspencerfake <106833326+funkspencerfake@users.noreply.github.com>
2022-06-10 16:09:38 -0400

    Update streamlist_app.py

--- streamlist_app.py
+++ streamlist_app.py
@@ -35,7 +35,7 @@ try:
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
   else:
-    back_from_function = rget_fruityvice_data(fruit_choice)
+    back_from_function = get_fruityvice_data(fruit_choice)
     #output it on the screen as a table
     streamlit.dataframe(back_from_function)
 
