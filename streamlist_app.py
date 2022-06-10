e86cc0443a04a0b92e39e20cc018e1c2c078c600
funkspencerfake <106833326+funkspencerfake@users.noreply.github.com>
2022-06-10 16:30:52 -0400

    Update streamlist_app.py

--- streamlist_app.py
+++ streamlist_app.py
@@ -54,14 +54,35 @@ except URLError as e:
 #output it on the screen as a table
 #streamlit.dataframe(fruityvice_normalized)
 
+streamlit.header("The fruit load list contains:")
+def get_fruit_load_list():
+    with my_cnx.cursor():
+        my_cur.execute("SELECT * from PC_RIVERY_DB.PUBLIC.fruit_load_list")
+        return my_cur.fetchall()
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from PC_RIVERY_DB.PUBLIC.fruit_load_list")
-my_data_row = my_cur.fetchall()
-streamlit.text("THe fruit load list contains:")
-streamlit.dataframe(my_data_row)
+if streamlit.button('Get Fruit Load List'):
+     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+     my_data_rows = get_fruit_load_list()
+     streamlit.dataframe(my_data_rows)
 
-fruit_add_choice = streamlit.text_input('What fruit would you like information about?', 'jackfruit')
-streamlit.text("Thanks for adding "+ fruit_add_choice) 
-my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.fruit_load_list values ('" + fruit_add_choice + "')")
+def insert_row_snowflake(new_fruit):
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.fruit_load_list values ('from streamlit')")
+        return "Thanks for adding " + new_fruit
+
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+if streamlit.button('Add a Fruit to the list'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    back_from_function = get_fruityvice_data(add_my_fruit)
+    streamlit.text(back_from_function)
+
+#my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+#my_cur = my_cnx.cursor()
+#my_cur.execute("SELECT * from PC_RIVERY_DB.PUBLIC.fruit_load_list")
+##my_data_row = my_cur.fetchall()
+#streamlit.text("THe fruit load list contains:")
+#streamlit.dataframe(my_data_row)
+
+#fruit_add_choice = streamlit.text_input('What fruit would you like information about?', 'jackfruit')
+#streamlit.text("Thanks for adding "+ fruit_add_choice) 
+#my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.fruit_load_list values ('" + fruit_add_choice + "')")
