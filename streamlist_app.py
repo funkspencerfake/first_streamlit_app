f89c5233b93dfa0dbfe246467e882be64d024c30
funkspencerfake <106833326+funkspencerfake@users.noreply.github.com>
2022-06-10 16:09:11 -0400

    Update streamlist_app.py

--- streamlist_app.py
+++ streamlist_app.py
@@ -24,15 +24,22 @@ streamlit.dataframe(fruits_to_show)
 
 #New Section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
+
+def get_fruityvice_data(this_fruit_choice):
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
+    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
+
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
   else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
-    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+    back_from_function = rget_fruityvice_data(fruit_choice)
     #output it on the screen as a table
-    streamlit.dataframe(fruityvice_normalized)
+    streamlit.dataframe(back_from_function)
+
+    
 
 #streamlit.write('The user entered', fruit_choice)
 
